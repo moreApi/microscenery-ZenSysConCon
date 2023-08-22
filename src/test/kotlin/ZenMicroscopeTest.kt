e7797f479fd92b7c67aa7e938ea379601d2250b4
Jan T <moreApi@users.noreply.github.com>
2023-08-22 11:32:48 +0200

    ZenMicroscopeTest: get document from zenBlue

--- src/test/kotlin/ZenMicroscopeTest.kt
+++ src/test/kotlin/ZenMicroscopeTest.kt
@@ -9,12 +9,16 @@ import microscenery.signals.Stack
 import microscenery.zenSysConCon.ZenBlueTCPConnector
 import microscenery.zenSysConCon.ZenMicroscope
 import microscenery.zenSysConCon.sysCon.SysConNamedPipeConnector
+import org.joml.Vector3f
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 
 class ZenMicroscopeTest {
@@ -49,22 +53,38 @@ class ZenMicroscopeTest {
     }
 
     @Test
-    fun readStack(){
+    fun readDebugStack(){
         zenMicroscope.debugStack(experimentFile.path)
         waitForMicroscopeToFinish()
-        assert(zenMicroscope.output.poll() is MicroscopeStatus)
-        assert(zenMicroscope.output.poll() is HardwareDimensions)
-        assert(zenMicroscope.output.poll() is Stack)
-        for (i in 1..5){
-            val s = zenMicroscope.output.poll()
-            assert(s is Slice)
-            assertEquals(i,(s as Slice).Id)
-        }
+        assertStackHasBeenLoaded()
+    }
+
+    @Test
+    fun getStackFromZenBlue(){
+        whenever(zenBlue.getCurrentDocument()).thenReturn(experimentFile.path)
+        zenMicroscope.snapSlice()
+        waitForMicroscopeToFinish()
+        assertStackHasBeenLoaded()
+        verify(zenBlue).runExperiment()
     }
 
     private fun waitForMicroscopeToFinish(){
         runBlocking { withTimeout(10000) {
             zenMicroscope.debugSync().acquire()
         }}
+    }
+
+    private fun assertStackHasBeenLoaded() {
+        assert(zenMicroscope.output.poll() is MicroscopeStatus)
+        assert(zenMicroscope.output.poll() is HardwareDimensions)
+        val stack = zenMicroscope.output.poll() as? Stack
+        assertNotNull(stack)
+        assertEquals(Vector3f(107f, 78.7f, -2f), stack.from)
+        assertEquals(2f, stack.to.z)
+        for (i in 1..5) {
+            val s = zenMicroscope.output.poll()
+            assert(s is Slice)
+            assertEquals(i, (s as Slice).Id)
+        }
     }
 }
