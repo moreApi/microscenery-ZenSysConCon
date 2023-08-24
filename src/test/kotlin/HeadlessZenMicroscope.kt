fe7f3b1934c88cd5a1c091cf296cafbab814cec4
Jan T <moreApi@users.noreply.github.com>
2023-08-24 10:15:15 +0200

    HeadlessZenMicoscope added

---
+++ src/test/kotlin/HeadlessZenMicroscope.kt
@@ -0,0 +1,47 @@
+package microscenery.example
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import microscenery.MicroscenerySettings
+import microscenery.lightSleepOnCondition
+import microscenery.setVector3
+import microscenery.signals.ClientSignal
+import microscenery.signals.Stack
+import microscenery.zenSysConCon.ZenMicroscope
+import org.joml.Vector3f
+
+class HeadlessZenMicroscope {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MicroscenerySettings.set("debug", true)
+            MicroscenerySettings.setVector3("Ablation.PrecisionUM",Vector3f(1f))
+            MicroscenerySettings.set("Ablation.SysCon.LightSourceId","dummy_0_0")
+            MicroscenerySettings.set("Ablation.SysCon.TriggerPort","UGA-42TTL In 1")
+            val zenMicroscope = ZenMicroscope()
+
+            zenMicroscope.snapSlice()
+            lightSleepOnCondition { zenMicroscope.output.poll()?.let {
+                return@let it is Stack
+            } ?: false }
+
+            zenMicroscope.ablatePoints(ClientSignal.AblationPoints(listOf(
+                ClientSignal.AblationPoint(Vector3f(0f,0f,0f)),
+                ClientSignal.AblationPoint(Vector3f(0f,0f,2f)),
+                ClientSignal.AblationPoint(Vector3f(0f,0f,2f))
+            )))
+//            while (true){
+//                val signale = zenMicroscope.output.take()
+//                println(signale)
+//            }
+            runBlocking {
+                launch {
+                    zenMicroscope.debugSync().acquire()
+                }
+            }
+
+
+
+        }
+    }
+}
